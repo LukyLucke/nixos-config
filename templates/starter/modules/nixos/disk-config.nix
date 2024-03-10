9bb46fe915b1b501bfe9935ae79ac31971bf0cfd
GabdaG <142120780+LukyLucke@users.noreply.github.com>
2024-03-10 10:24:49 +0300

    Update disk-config.nix

--- templates/starter/modules/nixos/disk-config.nix
+++ templates/starter/modules/nixos/disk-config.nix
@@ -22,7 +22,7 @@ _: {
               size = "100%";
               content = {
                 type = "filesystem";
-                format = "ext4";
+                format = "btrfs";
                 mountpoint = "/";
               };
             };
